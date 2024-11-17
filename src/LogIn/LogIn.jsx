0dd4596f04ea6f754156a7426e622c5728c852cf
Snu Seam <siamahme766@gmail.com>
2024-11-17 22:41:05 +0600

    commit

--- src/LogIn/LogIn.jsx
+++ src/LogIn/LogIn.jsx
@@ -7,6 +7,7 @@ const LogIn = () => {
     const [registerError ,setError] = useState('')
     // const [passError, setPassError] = useState('')
     const [success, setSuccess] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
     const auth = getAuth(app)
     
 
@@ -15,11 +16,16 @@ const LogIn = () => {
         console.log("Form Submitted");
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email,password);
+        const terms = e.target.terms.checked;
+        console.log(email,password,terms);
         setError('')
         setSuccess('')
         if(password.length < 6){
             setError('Password should be 6 character or longer')
+            return;
+        }
+        if(!terms){
+            setError('Please click terms and conditions')
             return;
         }
 
@@ -40,11 +46,15 @@ const LogIn = () => {
 
     return (
         <div >
+            <div>
             <h1 className="text-2xl mb-3">Register</h1>
             <form onSubmit={handleSubmit}>
             <input className="mb-2 p-2 rounded-lg" type="email" name="email" required id="" />
             <br />
-            <input className="mb-2 p-2 rounded-lg" type="password" name="password" required id="" />
+            <input className="mb-2 p-2 rounded-lg" type={showPassword ? 'text' : 'password'} name="password" required id="" /><span  onClick={()=> setShowPassword(!showPassword)} className="cursor-pointer absolute -ml-12 mt-2">Show</span>
+            <br />
+            <input type="checkbox" name="terms" id="terms" />
+            <label htmlFor="terms">accept our terms and <a href="">conditions</a></label>
             <br />
             <input className="btn" type="submit" value="LogIn" />
             </form>
@@ -54,6 +64,8 @@ const LogIn = () => {
             {
                 success && <p className="text-green-500">{success}</p>
             }
+            </div>
+            
         </div>
     );
 };
